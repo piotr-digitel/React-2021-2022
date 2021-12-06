071274f027550266dcbc86ba54f55685f8080102
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2021-12-06 21:18:14 +0100

    'ps3-zad2'

--- react2021/src/components/LeftColumn/LeftColumn.js
+++ react2021/src/components/LeftColumn/LeftColumn.js
@@ -1,35 +1,46 @@
-import React from 'react';
-import logo from '../.././logo.svg';
+import React from "react";
 // import styles from './LeftColumn.module.scss';
-import commonColumnsStyles from '../../common/styles/Columns.module.scss'
+import commonColumnsStyles from "../../common/styles/Columns.module.scss";
 
 class LeftColumn extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        counter: 0,
-      }
-      this.handleIncreaseCounter = this.handleIncreaseCounter.bind(this);
-    }
-
-    handleIncreaseCounter() {
-      this.setState( {
-        counter: this.state.counter + 1,
-       })
-    }
-    render() {
-      const { counter } = this.state;
-        return (
-            <div className={commonColumnsStyles.App}>
-              <header className={commonColumnsStyles.AppHeader}>
-                <button onClick={this.handleIncreaseCounter}> INCREASE BY 1 </button>
-                <p>
-                  {counter}
-                </p>
-              </header>
-            </div>
-          );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+    this.handleIncreaseCounter = this.handleIncreaseCounter.bind(this);
+    this.handleDecreaseCounter = this.handleDecreaseCounter.bind(this);
   }
 
-  export default LeftColumn;
+  handleIncreaseCounter() {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
+
+  handleDecreaseCounter(event) {
+    event.preventDefault();
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  }
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <div className={commonColumnsStyles.App}>
+        <header className={commonColumnsStyles.AppHeader}>
+          <button
+            onClick={this.handleIncreaseCounter}
+            onContextMenu={this.handleDecreaseCounter}
+          >
+            Left click + 1 / Right click -1
+          </button>
+          <p>{counter}</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default LeftColumn;
