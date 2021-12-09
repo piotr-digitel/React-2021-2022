4cf14136e48e2645423e2fae726b16801ec895d8
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2021-12-09 22:14:00 +0100

    ps3-zad7

--- react2021/src/components/RightColumn/RightColumn.js
+++ react2021/src/components/RightColumn/RightColumn.js
@@ -4,6 +4,9 @@ import commonColumnsStyles from '../../common/styles/Columns.module.scss'
 
 function RightColumn(props) {
   const [currentInputValue, setCurrentInputValue] = useState('')
+  const [name, setName] = useState('')
+  const [zodiac, setZodiac] = useState('')
+  const [messageText, setMessageText] = useState('')
   const buttonText = 'Double click should reset a timer';
 
   const handleDoubleClick = () => {
@@ -18,6 +21,20 @@ function RightColumn(props) {
     props.onSetTimerToValue(currentInputValue);
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setMessageText(`Cześć ${name}, twój znak zodiaku to: ${zodiac}`)
+  }
+
+  const handleChange = (event) => {
+    if (event.target.name === 'name') {
+      setName(event.target.value)
+    }
+    if (event.target.name === 'zodiac') {
+      setZodiac(event.target.value)
+    }
+  }
+
   return (
     <div className={commonColumnsStyles.App}>
       <header className={commonColumnsStyles.AppHeader}>
@@ -26,6 +43,18 @@ function RightColumn(props) {
         <button onClick={handleSetTimerFromValue}>
           Set value from input to timer
         </button>
+        <form onSubmit={handleSubmit}>
+            <label>
+              Imię:
+              <input type="text" name="name" value={name} onChange={handleChange} />
+            </label>
+            <label>
+              Znak zodiaku:
+              <input type="text" name="zodiac" value={zodiac} onChange={handleChange} />
+            </label>
+            <input type="submit" value="Wyślij" />
+          </form>
+          <p>{messageText}</p>
       </header>
     </div>
   );
