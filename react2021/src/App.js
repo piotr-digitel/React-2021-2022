0dd34818237f73128bf1d9a13ab1b76a6ca53741
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-01-15 11:13:50 +0100

    ps4-zad1

--- react2021/src/App.js
+++ react2021/src/App.js
@@ -2,8 +2,10 @@ import Results from "./components/Results/Results";
 // import RightColumn from './components/RightColumn/RightColumn';
 import Header from "./components/Header/Header";
 import styles from "./App.module.scss";
+import vehicles from "./common/consts/vehicles";
 
 function App() {
+  console.log('wszystkie pojazdy', vehicles)
   return (
     <div className={styles.appWrapper}>
       <Header />
