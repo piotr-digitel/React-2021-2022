349434cb5c31f286e50af92f8ff6a96f3ac91f0d
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-01-30 09:24:32 +0100

    template

--- react2021/src/App.js
+++ react2021/src/App.js
@@ -1,15 +1,17 @@
-import LeftColumn from './components/LeftColumn/LeftColumn';
-import RightColumn from './components/RightColumn/RightColumn';
-import Header from './components/Header/Header';
+import ProductsList from './components/ProductsList/ProductsList';
+import ShopingList from './components/ShopingList/ShopingList';
+import AddProducts from './components/AddProducts/AddProducts';
+import ProductsFilters from './components/ProductsFilters/ProductsFilters';
 import styles from './App.module.scss';
 
 function App() {
   return (
     <div className={styles.appWrapper}>
-      <Header />
+      <AddProducts />
+      <ProductsFilters />
       <div className={styles.columnsWrapper}>
-        <LeftColumn />
-        <RightColumn />
+        <ProductsList />
+        <ShopingList />
       </div>
     </ div>
   );
